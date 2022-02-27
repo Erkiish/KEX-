d9eabc5a84e61993a71ef85340880667ce55df80
Oliver Spjuth <oliver.spjuth@gmail.com>
2022-02-27 17:26:03 +0100

    .

--- ML/CONSTANSTS.py
+++ ML/CONSTANSTS.py
@@ -1,8 +1,8 @@
 import os
 
-directory_base_path = os.getcwd()
-PATH_ADDER = '/' if '/' in directory_base_path else '\\'
-ML_PATH = directory_base_path + PATH_ADDER + 'ML'
+DIRECTORY_BASE_PATH = os.getcwd()
+PATH_ADDER = '/' if '/' in DIRECTORY_BASE_PATH else '\\'
+ML_PATH = DIRECTORY_BASE_PATH + PATH_ADDER + 'ML'
 HP_TUNER_RESULT_PATH = ML_PATH + PATH_ADDER + 'HP_Tuner_Results'
 MODEL_EVAL_RESULTS_PATH = ML_PATH + PATH_ADDER + 'Model_Eval_Results'
 TENSORBOARD_LOG_FIT_PATH = ML_PATH + PATH_ADDER + 'TensorBoard_logs' + PATH_ADDER + 'fit_logs' + PATH_ADDER
